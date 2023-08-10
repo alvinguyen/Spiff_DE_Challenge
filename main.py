489f872c31e7b732e55744960822bf859133662e
alvinguyen <nguyen1alvin@gmail.com>
2023-08-10 15:10:20 -0500

    setting up env and main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+def calculate_commission(sales_rep_name, start_date, end_date):
+    """
+    Function/method to calculate commission for a sales rep in a given time period.
+
+    Args:
+        sales_rep_name (str): Name of the sales rep to calculate commission for.
+        start_date (str): Starting date for the date range where commissions will be valid.
+        end_date (str): Ending date for the date range where commissions will be valid.
+
+    Returns:
+        float: A single float value for total commission amount based on the input criteria. e.g. $749.48
+    """
+
+    ### Your code and call to helper functions/methods here ###
+    total_commission = "hello"
+    print(total_commission)
+
+    return total_commission
+    
+if __name__ == "__main__":
+    calculate_commission(sales_rep_name="Test", start_date="X", end_date="X")
